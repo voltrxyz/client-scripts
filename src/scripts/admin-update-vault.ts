0ef29ca8810619b4545c3b47ae9acbe31e345834
jakeyvee <jakeyveee@gmail.com>
2025-01-25 22:33:04 +0300

    feat: add update config for admin

---
+++ src/scripts/admin-update-vault.ts
@@ -0,0 +1,42 @@
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import * as fs from "fs";
+import { sendAndConfirmOptimisedTx } from "../helper";
+import { VoltrClient } from "@voltr/vault-sdk";
+import {
+  adminFilePath,
+  heliusRpcUrl,
+  vaultAddress,
+  vaultConfig,
+} from "../variables";
+
+const adminKpFile = fs.readFileSync(adminFilePath, "utf-8");
+const adminKpData = JSON.parse(adminKpFile);
+const adminSecret = Uint8Array.from(adminKpData);
+const adminKp = Keypair.fromSecretKey(adminSecret);
+const admin = adminKp.publicKey;
+
+const vault = new PublicKey(vaultAddress);
+
+const connection = new Connection(heliusRpcUrl);
+const vc = new VoltrClient(connection);
+
+const updateVaultHandler = async () => {
+  const createUpdateVaultIx = await vc.createUpdateVaultIx(vaultConfig, {
+    vault,
+    admin,
+  });
+
+  const txSig = await sendAndConfirmOptimisedTx(
+    [createUpdateVaultIx],
+    heliusRpcUrl,
+    adminKp
+  );
+
+  console.log(`Vault updated with signature: ${txSig}`);
+};
+
+const main = async () => {
+  await updateVaultHandler();
+};
+
+main();
--- src/variables.ts
+++ src/variables.ts
@@ -2,7 +2,7 @@ import { BN } from "@coral-xyz/anchor";
 import { VaultConfig, VaultParams } from "@voltr/vault-sdk";
 
 // ONLY NEEDED FOR INIT VAULT
-const vaultConfig: VaultConfig = {
+export const vaultConfig: VaultConfig = {
   maxCap: new BN(0), // 100K USDC (10^6 Decimals)
   startAtTs: new BN(0),
   managerPerformanceFee: -1, // 500 = 5% in basis points
